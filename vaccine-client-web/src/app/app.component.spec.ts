815954130c51823d42d984e7afaa40ff133d47c1
Manikanta Kuchipudi <mkuchipudi@modeln.com>
2021-01-11 12:08:35 +0530

    chnages

---
+++ vaccine-client-web/src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { Angulartics2Module } from 'angulartics2';
+
+import { CoreModule } from '@core';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let statusBarSpy: jasmine.Spy;
+  let splashScreenSpy: jasmine.Spy;
+  let keyboardSpy: jasmine.Spy;
+
+  beforeEach(
+    waitForAsync(() => {
+      statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
+      splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide']);
+      keyboardSpy = jasmine.createSpyObj('Keyboard', ['hideFormAccessoryBar']);
+
+      TestBed.configureTestingModule({
+        imports: [Angulartics2Module.forRoot(), RouterTestingModule, TranslateModule.forRoot(), CoreModule],
+        declarations: [AppComponent],
+        providers: [
+          { provide: Keyboard, useValue: keyboardSpy },
+          { provide: StatusBar, useValue: statusBarSpy },
+          { provide: SplashScreen, useValue: splashScreenSpy },
+        ],
+      }).compileComponents();
+    })
+  );
+
+  it(
+    'should create the app',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    }),
+    30000
+  );
+});
---
+++ vaccine-client/src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
+import { Angulartics2Module } from 'angulartics2';
+
+import { SharedModule } from '@shared';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  imports: [CommonModule, TranslateModule, SharedModule, IonicModule, Angulartics2Module, HomeRoutingModule],
+  declarations: [HomeComponent],
+})
+export class HomeModule {}
